907134f63f9c8766b21f8d4952869e0b3f881c98
Rakhi <1108rakhi@gmail.com>
2025-09-23 18:04:25 +0530

    routes related to users

--- users.py
+++ users.py
@@ -1,29 +1,31 @@
 from jose import jwt
 from datetime import datetime, timedelta, UTC
-# import os
-# from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+from . import models, database, schemas
+from fastapi import APIRouter, Depends, HTTPException
 
-users = [
-    {"id":1, "name":"Rakhi", "username":"Rakhi123","password":"secret1"},
-    {"id":2, "name":"Shivam", "username":"Shivam123","password":"secret2"},
-    {"id":3, "name":"Chetna", "username":"Chetna123","password":"secret3"}
-]
 # setting up jwt manually
 secret_key = 'mysecret'
 Algorithm = 'HS256'
 token_expiry = 20
 
+router = APIRouter()
+
 # creating token
-def create_token(username: str):
-    expire = datetime.now(UTC) + timedelta(minutes = token_expiry)
-    payload = {"sub":username, "exp":expire}
-    return jwt.encode(payload, secret_key, algorithm = Algorithm)
+def create_token(data:dict, expires_delta:timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode, secret_key, algorithm = Algorithm)
 
 # verifying user
-def verify_user(username : str, password: str):
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            return True
-    return False
+@router.post('/login')
+def login(request: schemas.LoginRequest, db:Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.username == request.username).first()
+    if not user or user.password != request.password:
+        raise HTTPException(status_code=401, detail = 'Invalid username or password')
+    
+    access_token = create_token(data = {"sub":user.username})
+    return {'access_token' : access_token, 'token_type': "bearer"}
 
 
