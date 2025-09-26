2e74dc43bf021092cf7a83103e6ea3e99347b7bd
Rakhi <1108rakhi@gmail.com>
2025-09-26 18:53:16 +0530

    added pydantic schema for glossary

--- schemas/schema.py
+++ schemas/schema.py
@@ -1,23 +1,46 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
-#login
+#user login
 class LoginRequest(BaseModel):
     username : str
     password : str
 
-#signup
+#user signup
 class CreateUser(BaseModel):
     name : str
     username : str
     password : str
     role : str='user'
 
-#get response
+#user get response
 class UserResponse(BaseModel):
     id : int
     name : str
     username : str
     role : str
 
+    class Config:
+        orm_mode = True
+
+# glossary creation
+class CreateGlossary(BaseModel):
+    term :str
+    description : str
+
+class UpdateGlossary(BaseModel):
+    term: Optional[str] = None
+    description : Optional[str] = None
+
+#glossary response
+class GlossaryResponse(BaseModel):
+    id : int
+    term:str
+    description : str
+    created_by : Optional[str] = None
+    updated_by : Optional[str]=None
+    created_at: Optional[datetime] = None
+    updated_at : Optional[datetime] = None
     class Config:
         orm_mode = True
