34b36d3c70883cea1e1ee50fff2ceaafcdbe957d
Rakhi <1108rakhi@gmail.com>
2025-09-26 18:58:35 +0530

    included glossary router in app

--- mymain.py
+++ mymain.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from myapp.routers import users
-from myapp.models import model
-from myapp.databases.database import engine
+from routers import users,glossaries
+from models import model
+from databases.database import engine
 
 model.Base.metadata.create_all(bind =engine)
 app = FastAPI()
 app.include_router(users.router)
 
+app.include_router(glossaries.router)
