302745fdffeaa4f66ae4c472dd61439a2d9dd64c
Rakhi <1108rakhi@gmail.com>
2025-10-15 13:13:57 +0530

    calling index creation function in main

--- mymain.py
+++ mymain.py
@@ -3,9 +3,10 @@ from router_users import users
 from router_glossary import glossaries
 from models import model
 from databases.database import engine
+from term_elasticsearch import create_index
 
 model.Base.metadata.create_all(bind =engine)
 app = FastAPI()
+create_index()
 app.include_router(users.router)
-
 app.include_router(glossaries.router)
